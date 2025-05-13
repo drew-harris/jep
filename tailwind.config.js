b24e39031ca9332704cc9e84b30e753e662e3a97
Drew Harris <drew@drewh.net>
2025-05-13 12:14:23 -0500

    FIX TAILWINDDD!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
   theme: {
-    extend: {
-      keyframes: {
-        "fade-in-out": {
-          "0%": { opacity: "0", transform: "translateY(-10px)" },
-          "10%": { opacity: "1", transform: "translateY(0)" },
-          "90%": { opacity: "1", transform: "translateY(0)" },
-          "100%": { opacity: "0", transform: "translateY(-10px)" },
-        },
-      },
-      animation: {
-        "fade-in-out": "fade-in-out 2s ease-in-out",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
